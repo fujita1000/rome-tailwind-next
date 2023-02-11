2900b0c8ee9af26e7acd9e1e1e9feaf4d509cfeb
{fujitanao} <fujitanao.rmt@gmail.com>
2023-02-11 13:40:47 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <h1 className='bg-blue-100 text-black'>ああああああああああああああ</h1>
+      </main>
+    </>
+  );
+}
